babad05ce0a99bce6be6d3a7721145744b65cc2b
Naoki Mita <naoki.mita18@gmail.com>
2024-08-04 22:19:12 -0700

    test(ToDoStats): add ToDoStats component

---
+++ packages/frontend/components/Recoil/ToDoStats/ToDoStats.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it } from 'vitest';
+
+describe('ToDoStats component', () => {
+  it.todo('Should show the total number of tasks');
+  it.todo('Should show the percentage of completed tasks');
+  it.todo('Should show the total number of over due tasks');
+});
