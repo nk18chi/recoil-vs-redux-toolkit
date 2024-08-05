9b74644a7ca372475d5dda9139ff23cd296ee995
Naoki Mita <naoki.mita18@gmail.com>
2024-08-04 23:10:57 -0700

    test(ToDoSearch): add todo unit tests for ToDoSearch component

---
+++ packages/frontend/components/ToDoSearch/ToDoSearch.test.tsx
@@ -0,0 +1,13 @@
+import { describe, it } from 'vitest';
+
+describe('ToDoSearch component', () => {
+  it.todo('Should show the text search box');
+  it.todo('Should search by the task title');
+  it.todo('Should show the category filter picker');
+  it.todo('Should filter by category');
+  it.todo('Should show the status filter picker');
+  it.todo('Should filter by status');
+  it.todo('Should show the due date filter picker');
+  it.todo('Should filter by due date');
+  it.todo('Should search all by text search, category filter, status filter and due date filter');
+});
