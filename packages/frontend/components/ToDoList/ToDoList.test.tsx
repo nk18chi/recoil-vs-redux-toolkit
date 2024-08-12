dfd5aab1d034e375f6467094d87584544680e536
Naoki Mita <naoki.mita18@gmail.com>
2024-08-11 21:49:35 -0700

    style(Lint): fix eslint issue in ToDoList

--- packages/frontend/components/ToDoList/ToDoList.test.tsx
+++ packages/frontend/components/ToDoList/ToDoList.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { screen, render } from '../../test-utils';
 import ToDoList from './ToDoList';
 
