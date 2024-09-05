50386eef7be030950a993b35815244b4342f8a9d
Naoki Mita <naoki.mita18@gmail.com>
2024-09-04 22:38:55 -0700

    refactor(FunctionalPrograming): use pipe from fp-ts for filtered todo list state

--- packages/frontend/state-managements/FilteredToDoListState.recoil.ts
+++ packages/frontend/state-managements/FilteredToDoListState.recoil.ts
@@ -1,5 +1,6 @@
 import { selector } from 'recoil';
 import ToDoListSearch from '@/classes/ToDoList/ToDoListSearch.class';
+import { pipe } from 'fp-ts/function';
 import todoListFiltersState from './ToDoListFiltersState.recoil';
 import ToDoListState from './ToDoListState.recoil';
 
@@ -9,12 +10,13 @@ const filteredTodoListState = selector({
     const todoList = get(ToDoListState);
     const filters = get(todoListFiltersState);
 
-    // TODO: use functional programming to chain filters
-    const newTodoList1 = ToDoListSearch.filterByStatus({ todoList, completed: filters.completed });
-    const newTodoList2 = ToDoListSearch.filterByCategory({ todoList: newTodoList1, category: filters.category });
-    const newTodoList3 = ToDoListSearch.searchByText({ todoList: newTodoList2, text: filters.searchText });
-    const newTodoList4 = ToDoListSearch.sortBy({ todoList: newTodoList3, sortBy: filters.sortBy });
-    return newTodoList4;
+    return pipe(
+      todoList,
+      (list) => ToDoListSearch.filterByStatus({ todoList: list, completed: filters.completed }),
+      (list) => ToDoListSearch.filterByCategory({ todoList: list, category: filters.category }),
+      (list) => ToDoListSearch.searchByText({ todoList: list, text: filters.searchText }),
+      (list) => ToDoListSearch.sortBy({ todoList: list, sortBy: filters.sortBy }),
+    );
   },
 });
 
